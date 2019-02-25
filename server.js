d2037f4daa4e5d37c52f06fc18546a3dccaa8c73
smk87 <30999133+smk87@users.noreply.github.com>
2019-02-25 19:43:53 +0600

    Update to run on heroku.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -17,10 +18,7 @@ app.use(bodyParser.json());
 const db = require("./config/key").mongoURI;
 //Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected."))
   .catch(err => console.log(err));
 
@@ -35,6 +33,14 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+//Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
